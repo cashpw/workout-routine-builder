25a6471fa6557545cdfd74f0ce213b56713bbbcd
Cash Weaver <cashbweaver@gmail.com>
2024-01-28 13:12:49 -0800

    :sparkles:: Define exercise data type

---
+++ app/src/exercises/exercise.d.ts
@@ -0,0 +1,9 @@
+import { Localization } from 'data/exercise';
+
+export interface Exercise {
+  // A unique identifier
+  id: number;
+
+  // A unique identifier
+  name: string;
+}
---
+++ app/src/exercises/exercises.ts
@@ -0,0 +1,16 @@
+import { Exercise } from 'data/exercise';
+
+export const barbellCurl: Exercise = {
+  id: 1,
+  name: 'barbellcurl',
+};
+
+export const barbellBenchPress: Exercise = {
+  id: 2,
+  name: 'barbellbenchpress',
+};
+
+export const barbellHipThrust: Exercise = {
+  id: 3,
+  name: 'barbellhipthrust',
+};
