59eab81060dbd292840aa90f985566b79ea3c05f
Cash Prokop-Weaver <cashweaver@google.com>
2024-01-30 08:59:58 -0800

    :sparkles:: Add header AppBar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Routine from 'components/Routine/Routine';
+import HeaderBar from 'components/HeaderBar/HeaderBar';
 import { Container } from '@mui/material';
 import 'App.css';
 
 function App() {
   return (
-    <Container
-      maxWidth="sm"
-    >
-      <Routine />
-    </Container>
+    <>
+      <HeaderBar />
+      <Container
+        maxWidth="sm"
+      >
+        <Routine />
+      </Container>
+    </>
   );
 }
 
---
+++ app/src/components/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,135 @@
+import * as React from 'react';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Button,
+  MenuItem,
+} from '@mui/material';
+import {
+  Menu as MenuIcon,
+} from '@mui/icons-material';
+
+const pages: string[] = [];
+
+function HeaderBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        mb: 2,
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            WRB
+          </Typography>
+
+          <Box sx={{
+            flexGrow: 1,
+            display: {
+              // xs: 'flex',
+              xs: 'none',
+              md: 'none',
+            }
+          }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            WRB
+          </Typography>
+          <Box sx={{
+            flexGrow: 1,
+            display: {
+              xs: 'none',
+              // md: 'flex',
+              md: 'none',
+            }
+          }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default HeaderBar;
