7ba25b682bdbb06c34768ae3c3c49d377a4dfb65
Cash Prokop-Weaver <cashweaver@google.com>
2024-01-30 16:33:43 -0800

    :sparkles:: Show weight repetition data

--- app/src/components/RepetitionItem/WeightRepetition.tsx
+++ app/src/components/RepetitionItem/WeightRepetition.tsx
@@ -1,17 +1,42 @@
 import type { WeightRepetition as WeightRepetitionType } from 'types';
+import { useTranslation } from 'react-i18next';
+import { WeightUnit } from 'types.d';
+
+import { Stack } from '@mui/material';
 
 export interface WeightRepetitionProps {
   repetition: WeightRepetitionType;
 }
 
 export default function WeightRepetition(props: WeightRepetitionProps) {
+  const { t } = useTranslation();
   const {
     repetition,
   } = props;
+  const {
+    weight,
+    count,
+    unit: weightUnit,
+  } = repetition;
+
+  const weightUnitAbbreviation: string = (() => {
+    switch (weightUnit) {
+      case WeightUnit.KILOGRAMS:
+        return t('kilogramsAbbreviation');
+      case WeightUnit.POUNDS:
+        return t('poundsAbbreviation');
+    }
+
+    // TODO Handle error case
+    return ""
+  })();
 
   return (
-    <>
-      {repetition.toString()}
-    </>
+    <Stack
+      direction="row"
+    >
+      <span>Weight: {weight} {weightUnitAbbreviation}</span>
+      <span>Count: {count}</span>
+    </Stack>
   );
 }
